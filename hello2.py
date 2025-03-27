fa080ee6f4186775530f2b16f05b832b0a11ddd8
Prabin1-hub <prabinkharel888@gmail.com>
2025-03-27 09:16:52 +0000

    learning

---
+++ hello2.py
@@ -0,0 +1,7 @@
+ 
+
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+print  (f"hello {name}. You are {age} years old.")
---
+++ hello3.py
@@ -0,0 +1,8 @@
+amount_usd = float(input("Enter the amount in USD: "))
+
+rate = 0.93
+
+amount_euro = amount_usd * rate
+
+
+print(f"amount in Euro {amount_euro}")
