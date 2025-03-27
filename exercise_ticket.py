94ab071b112395442f3b556ce05524c369ddbe40
Prabin1-hub <prabinkharel888@gmail.com>
2025-03-27 10:15:13 +0000

    ticket

---
+++ exercise_ticket.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if (age >= 18):
+    print ("Ticket price is $10")
+elif (age >= 4 & age < 18):
+    print ("Ticket price is $8")
+else:
+    print ("ticket price $5")
