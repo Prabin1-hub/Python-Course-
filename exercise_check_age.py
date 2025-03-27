1d8fdcb426227aef3206689d635e8a9c2df983bb
Prabin1-hub <prabinkharel888@gmail.com>
2025-03-27 10:02:34 +0000

    age

---
+++ exercise_check_age.py
@@ -0,0 +1,8 @@
+user_name = input("Enter your user name: ")
+age = int(input("Enter your age: "))
+
+
+if (age < 18):
+    print ("Our game is not suitable under the age if 18")
+else:
+    print (f"Welcome to our website {user_name}")
