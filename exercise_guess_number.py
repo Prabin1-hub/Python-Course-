7ede836b1cea5dee5b6aaf38920b3f9048665acd
Prabin1-hub <prabinkharel888@gmail.com>
2025-04-02 09:49:14 +0000

    number

--- exercise_guess_number.py
+++ exercise_guess_number.py
@@ -1,12 +1,15 @@
 import random
 
-number = random.randint(0, 100)
+number = random.randint(0, 10)
 while True:
-    guess = int(input("Guess a number (0-100): "))
-    if guess < number:
-        print("Too low!")
-    elif guess > number:
-        print("Too high!")
-    else:
-        print("Correct!")
-        break
+    try:
+        guess = int(input("Guess a number (0-10): "))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print("Correct!")
+            break
+    except ValueError:
+        print("Invalid, please enter only numbers")
