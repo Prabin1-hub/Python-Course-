0ed3beb0b3a74921a5c1df2e3d12f077f2917cd2
Prabin1-hub <prabinkharel888@gmail.com>
2025-04-03 09:55:36 +0000

    dictionnary

---
+++ exercise_dictionnary.py
@@ -0,0 +1,10 @@
+student = {
+    "first_name":  "prabin",
+     "last_name":  " kharel",
+      "age":  23,
+}
+
+print(f"student last_name {student["last_name"]}")
+print(f"student first_name {student["first_name"]}")
+print(f"student age {student["age"]}")
+
